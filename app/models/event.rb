233dfed232b9f92eed5462408b2237ccb57e94ce
Rob Wilson <rob.wilson@salesloft.com>
2019-01-22 00:51:14 -0500

    Validates Event attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+  validates_presence_of :uuid, 
+                        :start_time, 
+                        :end_time,
+                        :duration,
+                        :created_at,
+                        :invitees_count,
+                        :location,
+                        :canceled,
+                        :canceler_name,
+                        :canceled_at
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Event, type: :model do
   it { is_expected.to validate_presence_of(:uuid) }
   it { is_expected.to validate_presence_of(:start_time) }
   it { is_expected.to validate_presence_of(:end_time) }
-  
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:duration) }
+  it { is_expected.to validate_presence_of(:created_at) }
+  it { is_expected.to validate_presence_of(:invitees_count) }
+  it { is_expected.to validate_presence_of(:location) }
+  it { is_expected.to validate_presence_of(:canceled) }
+  it { is_expected.to validate_presence_of(:canceler_name) }
+  it { is_expected.to validate_presence_of(:canceled_at) }
 end
