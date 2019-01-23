554f287c5c9cd6ca7961852a5794f4c18103b31c
Rob Wilson <rob.wilson@salesloft.com>
2019-01-23 04:40:39 -0500

    Allows for nullable cancellation fields

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,12 +3,11 @@ FactoryBot.define do
     uuid { Faker::Lorem.word }
     start_time { 6.hours.ago }
     end_time { 5.hours.ago }
-    duration { 1 }
+    duration { 15 }
     created_at { 24.hours.ago }
-    invitees_count { 3 }
     location { Faker::Lorem.word }
-    canceled { true }
-    canceler_name { "foobar" }
-    canceled_at { 4.hours.ago }
+    canceled { false }
+    canceler_name { nil }
+    canceled_at { nil }
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -6,9 +6,4 @@ RSpec.describe Event, type: :model do
   it { is_expected.to validate_presence_of(:end_time) }
   it { is_expected.to validate_presence_of(:duration) }
   it { is_expected.to validate_presence_of(:created_at) }
-  it { is_expected.to validate_presence_of(:invitees_count) }
-  it { is_expected.to validate_presence_of(:location) }
-  it { is_expected.to validate_presence_of(:canceled) }
-  it { is_expected.to validate_presence_of(:canceler_name) }
-  it { is_expected.to validate_presence_of(:canceled_at) }
 end
